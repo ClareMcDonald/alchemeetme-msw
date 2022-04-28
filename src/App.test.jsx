82ab1eccfffdb066ef48e252e3d10da9098c78ac
ClareMcDonald <clare.s.mcdonald@gmail.com>
2022-04-28 10:41:05 -0700

    first test passes

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -23,45 +23,54 @@ const user = {
 
 // 🚨 Create your server
 const server = setupServer(
-  rest.get(`${process.env.REACT_APP_SUPABASE_URL}/rest/v1/users?select=*`, (req, res, ctx) => res(ctx.json(user)))
+  rest.get('https://uzgiamkrbapxufnwdrja.supabase.co/rest/v1/users', (req, res, ctx) => res(ctx.json([user])))
 );
 
-// 🚨 Listen for server start
-beforeAll(() => server.listen());
+describe('App', () => {
 
-// 🚨 Close server when complete
-afterAll(() => server.close());
-
-test('Should render the header', async () => {
-  render(<App />)
-  const banner = screen.getByRole('banner')
-  const headerImg = screen.getByAltText(/alchemy/i)
-  const profileName = await screen.findByText(user.name)
-
-  expect(banner).toHaveStyle({
-    background: 'var(--grey)',
+  // 🚨 Listen for server start
+  beforeAll(() => server.listen());
+  
+  // 🚨 Close server when complete
+  afterAll(() => server.close());
+  
+  test('Should render the header', async () => {
+    render(<App />)
+    const banner = screen.getByRole('banner')
+    const headerImg = screen.getByAltText(/alchemy/i)
+    const profileName = await screen.findByText(user.name)
+    
+    expect(banner).toHaveStyle({
+      background: 'var(--grey)',
+    })
+    expect(headerImg).toBeInTheDocument()
+    expect(profileName).toBeInTheDocument()
   })
-  expect(headerImg).toBeInTheDocument()
-  expect(profileName).toBeInTheDocument()
-})
-
-test('Should render the header with Sasuke 🌬️🔥', async () => {
-  const sasuke = {
-    id: 1,
-    created_at: '2021-12-13T00:17:29+00:00',
-    name: 'Sasuke 🌬️🔥',
-    avatar: 'https://thumbs.gfycat.com/NiceRequiredGrunion-size_restricted.gif',
-    header: 'https://static.wikia.nocookie.net/naruto/images/5/50/Team_Kakashi.png',
-    likes: ['React', 'Anime', 'Traveling', 'Living', 'Tower Defense Games', 'Card Games'],
-    motto: 'Res Non Verba',
-    color: 'crimson',
-  }
-
-  // 🚨 Use the server to change the response for this test
-
-  render(<App />)
-
-  const profileName = await screen.findByText(sasuke.name)
-
-  expect(profileName).toBeInTheDocument()
-})
+  
+  test('Should render the header with Sasuke 🌬️🔥', async () => {
+    const sasuke = {
+      id: 1,
+      created_at: '2021-12-13T00:17:29+00:00',
+      name: 'Sasuke 🌬️🔥',
+      avatar: 'https://thumbs.gfycat.com/NiceRequiredGrunion-size_restricted.gif',
+      header: 'https://static.wikia.nocookie.net/naruto/images/5/50/Team_Kakashi.png',
+      likes: ['React', 'Anime', 'Traveling', 'Living', 'Tower Defense Games', 'Card Games'],
+      motto: 'Res Non Verba',
+      color: 'crimson',
+    }
+    
+    server.use(
+      rest.get('https://uzgiamkrbapxufnwdrja.supabase.co/rest/v1/users', (req, res, cxt) => {
+        res(cxt.json([sasuke]));
+      })
+    )
+    // 🚨 Use the server to change the response for this test
+    
+    render(<App />)
+    
+    const profileName = await screen.findByText(sasuke.name)
+    
+    expect(profileName).toBeInTheDocument()
+  })
+  
+});
